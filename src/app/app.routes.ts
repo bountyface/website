521c6f32ed1fddb5b42567678ab62f5f6c7c7554
Michael Ebneth <michael.ebneth@gmx.de>
2024-01-22 21:18:18 +0100

    feat: :sparkles: add components and routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { HomeComponent } from './home/home.component';
+import { MyMusicComponent } from './my-music/my-music.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'about-me', component: AboutMeComponent },
+  { path: 'my-music', component: MyMusicComponent },
+];
