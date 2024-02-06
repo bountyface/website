0dcfa6e43ee051ce4ea7904d100642bd05cfcb72
Michael Ebneth <michael.ebneth@gmx.de>
2024-02-06 22:36:11 +0100

    feat: :sparkles: add footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from './footer/footer.component';
 import { INavigationItem } from './interfaces/navigation-item.model';
 import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavigationBarComponent],
+  imports: [RouterOutlet, NavigationBarComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
