bc29fea802e0e82dfb796a4e0a6f2ba84017933e
Michael Ebneth <michael.ebneth@gmx.de>
2024-03-18 22:15:47 +0100

    feat: :sparkles: add custom accordion

--- src/app/about-me/about-me.component.ts
+++ src/app/about-me/about-me.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { AccordionComponent } from '../components/accordion/accordion.component';
 
 @Component({
   selector: 'app-about-me',
   standalone: true,
-  imports: [],
+  imports: [AccordionComponent],
   templateUrl: './about-me.component.html',
-  styleUrl: './about-me.component.scss'
+  styleUrl: './about-me.component.scss',
 })
-export class AboutMeComponent {
-
-}
+export class AboutMeComponent {}
---
+++ src/app/components/accordion/accordion.component.ts
@@ -0,0 +1,41 @@
+import {
+  animate,
+  state,
+  style,
+  transition,
+  trigger,
+} from '@angular/animations';
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-accordion',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './accordion.component.html',
+  styleUrl: './accordion.component.scss',
+  animations: [
+    trigger('openClose', [
+      state(
+        'open',
+        style({
+          // height: '100px',
+        })
+      ),
+      state(
+        'closed',
+        style({
+          height: '0px',
+        })
+      ),
+      transition('open => closed', [animate('0.1s')]),
+      transition('closed => open', [animate('0.25s')]),
+    ]),
+  ],
+})
+export class AccordionComponent {
+  isOpen = false;
+  public onAccordionClick(): void {
+    this.isOpen = !this.isOpen;
+  }
+}
